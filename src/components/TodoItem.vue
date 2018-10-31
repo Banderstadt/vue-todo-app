1811a73a259c2008eef2234cb4853f9bf9f33789
Banderstadt <banderstadt@lwo-vl0020-191.corp.dtm.io>
2018-10-31 18:39:10 +0200

    added edit, toggle, dlete todo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todo-item" style="display: flex; justify-content: space-between">
+    <v-text-field v-model="todo" v-bind:class="[done ? 'is-done' : '']" @keyup="editTodo"></v-text-field>
+      <v-btn icon @click="toggleTodo">
+        <v-icon>done</v-icon>
+      </v-btn>
+      <v-btn icon @click="deleteTodo">
+        <v-icon>delete_forever</v-icon>
+      </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  data: () => ({
+    todo: "New todo",
+    done: false
+  }),
+  methods: {
+    deleteTodo () {
+      this.todo = '';
+    },
+    toggleTodo () {
+      this.done = !this.done;
+    },
+    editTodo () {
+      console.log(this.todo);
+    },
+  }
+}
+</script>
+
+<style>
+  .is-done {
+    text-decoration: line-through;
+  }
+</style>
