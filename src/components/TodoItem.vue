530835b28004439e236c181350bd4c7447d3151e
Banderstadt <banderstadt@lwo-vl0020-217.corp.dtm.io>
2018-11-01 16:36:41 +0200

    refactored

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="todo-item" style="display: flex; justify-content: space-between">
-    <v-text-field ref = 'text' v-model = 'title'
+    <v-text-field ref = 'text'
+                  v-model = 'title'
                   :value="todo.title"
                   v-bind:class="[todo.done ? 'is-done' : '']"
                   @blur="editTodo({todo, title})"
                   @keyup.enter="editTodo({todo, title})">
     </v-text-field>
-      <v-btn icon @click="toggleTodo(todo)">
-        <v-icon>done</v-icon>
-      </v-btn>
-      <v-btn icon @click="deleteTodo(todo)">
-        <v-icon>delete_forever</v-icon>
-      </v-btn>
+    <v-btn icon @click="toggleTodo(todo)">
+      <v-icon>done</v-icon>
+    </v-btn>
+    <v-btn icon @click="deleteTodo(todo)">
+      <v-icon>delete_forever</v-icon>
+    </v-btn>
   </div>
 </template>
 
