2c3ebe4626e1b8488f8e67cf0d3a8a8617c2cb6a
Serhii Silov <serhiikulaha@gmail.com>
2018-11-02 15:08:17 +0200

    d

---
+++ src/components/InputFeild.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-form>
+    <v-text-field v-model="newItemTitle"
+                    label="What do you need to do?"
+                    @keydown.enter.prevent="addTodo"
+                    autofocus
+                    required>
+    </v-text-field>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'InputFeild',
+  data () {
+    return {
+      newItemTitle: ''
+    }
+  },
+  computed: {
+  },
+  methods: {
+    addTodo () {
+      this.$store.dispatch('addTodo', this.newItemTitle)
+      this.newItemTitle = null
+    }
+  }
+}
+</script>
